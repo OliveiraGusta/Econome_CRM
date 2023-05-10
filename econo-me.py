42e42a6d51814a105ff4045f33f5156c4ab1e54f
MATEUS MARANA ASSUENA <unifmassuena@feinet.fei.edu.br>
2023-05-10 20:42:35 -0300

    Reformulamos o codigo inteiro, para melhor funcionamento da interface

--- econo-me.py
+++ econo-me.py
@@ -1,136 +1,140 @@
-## Gustavo Rogrigues e Mateus Marana
+# Gustavo Rodrigues e Mateus Marana
+appBanco = {
+'usuarios': [],
+'contas': []
+}
 
-# Dicionário com os clientes // Chave = Cpf
-# Lista outras informações
-
-clientes = { }
-informacoesClientes = []
-
-## Função 1 
-
-cpf = ""
-nome = ""
-conta = ""
-valor = ""
-senha = ""
-
-
-
-def novoCliente():
-
-    global cpf
-
-    global nome
-    global conta
-    global valor
-    global senha
-
-    cpf = input('Digite o CPF do cliente: ')
-    nome = input('Digite o nome completo do cliente: ')
-    conta = input('Digite o tipo de conta: ')
-    valor = input('Digite o valor inicial da conta: ')
-    senha = input('Digite a senha do usuário: ')
-
-    informacoesClientes.append(nome)
-    informacoesClientes.append(conta)
-    informacoesClientes.append(valor)
-    informacoesClientes.append(senha)
-
-    clientes[cpf] = informacoesClientes
-   
-    print('Usuario cadastrado com SUCESSO!!')
-    return
-
-# Função apagar cliente
-def apagarCliente():
+#Função Cadastrar novo Clientes Cadastrados
+def novoCliente(nome,senha,cpf,tipoConta):
     print()
-    cpf = input('Digite o CPF a ser deletado: ')
-    for x in clientes:
-        if x == cpf:
-            remover = input("Deseja realmente apagar o ccleliente pertencente ao CPF %s \n \nDigite 1 para confirmar \n \nDigite 2 para confirmar cancelar\n" % (cpf) )
-            if(remover == "1"):
-               #Apagar
-                print("Tudo bem, cliente com CPF %s removido" % (cpf)) 
-                del clientes[cpf]
-                break
-            else:
-                #Nao Apagar
-                print("Tudo bem, cliente não removido")  
-                break   
-        else:
-            #cpf nao existe
-            print("CPF nao condiz a algum cliente cadastrado")
-            break
-        
+    usuario = {
+    'nome': nome,
+    'senha': senha,
+    'cpf' : cpf
+    }
+    conta = {
+    'usuario': usuario,
+    'saldo': 0,
+    'tipoConta': tipoConta
+    
+    }
+    appBanco['usuarios'].append(usuario)
+    appBanco['contas'].append(conta)
 
-# Função Exibir os Clientes Cadastrados
+
+#Função Apagar os Clientes Cadastrados
+def apagarCliente(cpf):
+    print()
+    for i, usuario in enumerate(appBanco['usuarios']):
+        if usuario['cpf'] == cpf:
+            del appBanco['usuarios'][i]
+            del appBanco['contas'][i]
+            print("Usuário excluído com sucesso!")
+        return
+        print("Usuário não encontrado.")
+
+#Função Exibir os Clientes Cadastrados
 def listarClientes():
-    if clientes == {}:
-        print("Não há clientes cadastrados")
-    else:
-        print("Listagem de Clientes Cadastrados")
-        print('--------------------')
-        for x,y in clientes.items():
-                print('CPF = %s : %s' % (x,y))
-                break
-    
-    
+    print()
+    for usuario in appBanco['usuarios']:
+        print(f"Nome: {usuario['nome']}")
+        print(f"Cpf: {usuario['cpf']}")
 
-# Funçã Debitar Qualquer Valor
+#Função Debitar Qualquer Valor
 def debitarValor():
     print()
     print("Entrou na funcao Debitar Valor")
-    
-# Funçã Depositar Qualquer Valor
+
+#Função Depositar Qualquer Valor
 def depositaValor():
     print()
     print("Entrou na funcao Deposita Valor")
-    
+
 #Função Exibir Extrato da Conta
 def extratoDaConta():
     print()
     print("Entrou na funcao Extrato da Conta ")
    
 #Função Trasferir o Valor
-def transferirValor():
+def transferirValor(origem,destino,valor):
     print()
-    print("Entrou na funcao Transferir Valor")
+    conta_origem = None
+    conta_destino = None
+
+    for conta in appBanco['contas']:
+        if conta['usuario']['nome'] == origem:
+            conta_origem = conta
+        if conta['usuario']['nome'] == destino:
+            conta_destino = conta
+
+        if conta_origem is None:
+            print("Conta de origem não encontrada.")
+        return
+        if conta_destino is None:
+            print("Conta de destino não encontrada.")
+        return
+
+        if conta_origem['saldo'] >= valor:
+            conta_origem['saldo'] -= valor
+            conta_destino['saldo'] += valor
+            print("Transferência realizada com sucesso!")
+        else:
+            print("Saldo insuficiente para realizar a transferência.")
    
 #Função para Acessar os Investimentos
 def investimentos():
     print()
     print("Entrou na funcao Investimentos")
    
-
-def sair():
-    print()
-    print("Obrigado por utilizar nosso programa")
-    
-
-
 #Painel
 while True:
     print('--------------------')
     print('$$ ECONO-ME BANK $$')
     print('--------------------')
     print()
-    print('Para Cadastrar Novo Cliente - Digite 1')
-    print('Para Apagar Cliente pelo Cpf - Digite 2')
-    print('Para Listar os Clientes Existentes - Digite 3')
-    print('Para Debitar um Valor na Conta - Digite 4')
-    print('Para Depositar um Valor na Conta - Digite 5')
-    print('Para Tirar Extrato - Digite 6')
-    print('Para Transferir um Valor -Digite 7')
-    print('Para Investir um Valor - Digite 8')
-    print('Para Fechar o programa - Digite 9')
+    print(' Digite 1 - Cadastrar Novo Cliente  ')
+    print(' Digite 2 - Apagar Cliente pelo Cpf')
+    print(' Digite 3 - Listar os Clientes Existentes')
+    print(' Digite 4 - Debitar um Valor na Conta')
+    print(' Digite 5 - Depositar um Valor na Conta')
+    print(' Digite 6 - Tirar Extrato')
+    print(' Digite 7 - Transferir um Valor')
+    print(' Digite 8 - Investir um Valor')
+    print(' Digite 9 - Fechar o programa')
     print()
     decisao = input('O que Deseja?: ')
-    if decisao == '1': novoCliente()
-    if decisao == '2': apagarCliente()
-    if decisao == '3': listarClientes()
-    if decisao == '4': debitarValor()
-    if decisao == '5': depositaValor()
-    if decisao == '6': extratoDaConta()
-    if decisao == '7': transferirValor()
-    if decisao == '8': investimentos()
-    if decisao == '9': break
+    
+    if decisao == '1':  
+        nome = input(" Digite o nome do usuário: ")
+        cpf = input(" Digite o cpf do usuário:")
+        tipoConta = input(" Digite o tipo da conta:")
+        senha = input(" Digite a senha do usuário: ")
+        novoCliente(nome, senha, cpf, tipoConta)
+    
+    if decisao == "2":
+        cpf = input(" o cpf do usuário a ser excluído: ")
+        apagarCliente(cpf)
+    
+    if decisao == '3': 
+        listarClientes()
+    
+    if decisao == '4':
+         debitarValor()
+    
+    if decisao == '5':
+         depositaValor()
+    
+    if decisao == '6': 
+        extratoDaConta()
+    
+    if decisao == '7':  
+        origem = input(" o nome da conta de origem: ")
+        destino = input(" o nome da conta de destino: ")
+        valor = float(input(" o valor da transferência: "))
+        transferirValor(origem, destino, valor)
+    
+    if decisao == '8': 
+        investimentos()
+    
+    if decisao == '9': 
+        break
